a4bd41cce77797eb26071cf6a48f7b377070ba97
Amr Magdi <Amr.Magdi.M@gmail.com>
2019-08-16 15:14:25 +0200

    Adding generic libraries

---
+++ Src/LIB/std_types.h
@@ -0,0 +1,21 @@
+/*
+ * std_types.h
+ *
+ *  Created on: Aug 16, 2019
+ *      Author: Amr
+ */
+
+#ifndef NASR5_REPO_SRC_LIB_STD_TYPES_H_
+#define NASR5_REPO_SRC_LIB_STD_TYPES_H_
+
+typedef unsigned char	uint8;
+
+typedef enum
+{
+	STD_LOW, /* 0 */
+	STD_HIGH /* 1 */
+} STD_levelType;
+
+
+
+#endif /* NASR5_REPO_SRC_LIB_STD_TYPES_H_ */
