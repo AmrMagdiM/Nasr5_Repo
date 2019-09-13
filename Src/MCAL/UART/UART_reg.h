3efe48e310aeeba1e32f06d23ef55a71fbd0ad80
Amr Magdi <Amr.Magdi.M@gmail.com>
2019-09-13 14:05:42 +0200

    adding parentheses to define casting

--- Src/MCAL/UART/UART_reg.h
+++ Src/MCAL/UART/UART_reg.h
@@ -8,10 +8,10 @@
 #ifndef SRC_MCAL_UART_UART_REG_H_
 #define SRC_MCAL_UART_UART_REG_H_
 
-#define  UDR_Reg		   *(volatile uint8* 0x2C)
-#define  USCRA_Reg         *(volatile uint8* 0x2B)
-#define  USCRB_Reg         *(volatile uint8* 0x2A)
-#define  USCRC_Reg         *(volatile uint8* 0x40)
-#define  USART_Reg         *(volatile uint8* 0x29)
+#define  UDR_Reg		   (*((volatile uint8*) 0x2C)
+#define  USCRA_Reg         *((volatile uint8*) 0x2B)
+#define  USCRB_Reg         *((volatile uint8*) 0x2A)
+#define  USCRC_Reg         *((volatile uint8*) 0x40)
+#define  USART_Reg         *((volatile uint8*) 0x29)
 
 #endif /* SRC_MCAL_UART_UART_REG_H_ */
