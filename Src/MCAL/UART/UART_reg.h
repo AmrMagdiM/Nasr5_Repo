fa5b3671179af2a0aa76b8d61fa18c05e79b85a4
Amr Magdi <Amr.Magdi.M@gmail.com>
2019-09-13 14:06:38 +0200

    fixing parantheses

--- Src/MCAL/UART/UART_reg.h
+++ Src/MCAL/UART/UART_reg.h
@@ -8,7 +8,7 @@
 #ifndef SRC_MCAL_UART_UART_REG_H_
 #define SRC_MCAL_UART_UART_REG_H_
 
-#define  UDR_Reg		   (*((volatile uint8*) 0x2C)
+#define  UDR_Reg		   *((volatile uint8*) 0x2C)
 #define  USCRA_Reg         *((volatile uint8*) 0x2B)
 #define  USCRB_Reg         *((volatile uint8*) 0x2A)
 #define  USCRC_Reg         *((volatile uint8*) 0x40)
